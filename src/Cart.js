15d2e0c3573a6e7deebccf4366750f43d1ed22e4
Omkar Deshmukh <iamomkar666@gmail.com>
2021-05-11 12:12:24 +0530

    Training GH first

---
+++ src/Cart.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Cart = () => {
+
+    return(
+        <div>
+            <h3>You surely have selected something.</h3>
+        </div>
+    )
+}
+
+export default Cart
