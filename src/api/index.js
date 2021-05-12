c96219960034851bde3b7525fbe728ba80cd3117
Omkar Deshmukh <iamomkar666@gmail.com>
2021-05-12 15:29:17 +0530

    3

---
+++ src/api/index.js
@@ -0,0 +1,32 @@
+{
+    [
+        {
+            productName: "Pen",
+            price: 10,
+            currency: "rupees",
+            description: "A small description on pen.",
+            quanityAvailable: 35,
+        },
+        {
+            productName: "Pencil Box",
+            price: 250,
+            currency: "rupees",
+            description: "A small description on pencil box.",
+            quanityAvailable: 50,
+        },
+        {
+            productName: "Touch Pen",
+            price: 1849,
+            currency: "rupees",
+            description: "A small description on touch pen.",
+            quanityAvailable: 12,
+        },
+        {
+            productName: "Ink (Black)",
+            price: 300,
+            currency: "rupees",
+            description: "A small description on ink pods.",
+            quanityAvailable: 20,
+        }
+    ]
+}
